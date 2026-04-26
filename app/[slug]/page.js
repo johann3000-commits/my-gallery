ce5bfd2d32baabe9583d3a456c9c0abe7b9aee2a
Johann Kabonen <johann3000@Johanns-MacBook-Pro.local>
2026-04-26 20:31:41 +0300

    dynamic homepage

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,32 +1,20 @@
-import GalleryClient from "./GalleryClient";
 import { client } from "@/lib/sanity";
-import { notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 
-export default async function Page({ params }) {
-  const { slug } = await params;
-
+export default async function Home() {
   const galleries = await client.fetch(`
     *[_type == "gallery"] | order(order asc) {
-      title,
-      subtitle,
-      "slug": slug.current,
-      images
+      "slug": slug.current
     }
   `);
 
-  const currentIndex = galleries.findIndex(
-    (g) => g.slug === slug
-  );
-
-  // ❌ kui slug ei klapi → 404
-  if (currentIndex === -1) {
-    notFound();
+  // kui pole ühtegi galeriid
+  if (!galleries || galleries.length === 0) {
+    return <div>No galleries found</div>;
   }
 
-  return (
-    <GalleryClient
-      galleries={galleries}
-      currentIndex={currentIndex}
-    />
-  );
+  // võta esimene
+  const firstSlug = galleries[0].slug;
+
+  redirect(`/${firstSlug}`);
 }
